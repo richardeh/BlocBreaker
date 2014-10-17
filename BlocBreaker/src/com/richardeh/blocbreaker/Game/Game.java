11b3bd9a3518a5eaa21151d41e7ce21113f34ef2
Richard Harrington <richardharrington@gmail.com>
2014-10-16 19:25:12 -0500

    ball bounce test working

--- BlocBreaker/src/com/richardeh/blocbreaker/Game/Game.java
+++ BlocBreaker/src/com/richardeh/blocbreaker/Game/Game.java
@@ -1,40 +1,111 @@
 package com.richardeh.blocbreaker.Game;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.XmlReader;
 
 /**
  * Created by Richard Harrington on 10/4/2014.
  */
 public class Game extends com.badlogic.gdx.Game{
-    private Ball ball;
     private Paddle paddle;
     private int currentLevel;
+    private float w,h;
+    private ArrayList<Block> blocks;
+    private static ArrayList<Ball> balls;
 
     public Game(){
-        ball = new Ball(0,0,32,32,0,0,0,0);
-        paddle = new Paddle(0,0,64,32,0,0);
+        Ball ball = new Ball(0,0,32,32,5,5,0,0);
+        Ball ball2 = new Ball(0,0,32,32,10,10,0,0);
+        balls = new ArrayList<Ball>();
+        balls.add(ball);
+        balls.add(ball2);
+        
+        paddle = new Paddle(0,0,104,24,0,0);
         currentLevel = 1;
-        loadLevel(currentLevel);
+        blocks= new ArrayList<Block>();
+        		//loadLevel(currentLevel);
     }
 
-    private void loadLevel(int level){
+    private ArrayList<Block> loadLevel(int level){
         //Load the current level
         // TODO: fill in
         // TODO: determine a file format
-        Gdx.files.internal("levels/"+level+".xml");
+        XmlReader reader = new XmlReader();
+        try {
+			reader.parse(Gdx.files.internal("levels/"+level+".xml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return new ArrayList<Block>();
+    }
+    
+    public void moveBall(Ball b,Vector2 v){
+
+    		b.position.add(v);
+
     }
 
-    public Vector2 getBallPosition(){
-        return ball.position;
+    public ArrayList<Vector2> getBallPosition(){
+    	ArrayList<Vector2> positions = new ArrayList<Vector2>();
+    	for(Ball b:balls){
+    		positions.add(b.position);
+    	}
+        return positions;
     }
 
     public Vector2 getPaddlePosition(){
         return paddle.position;
     }
+    
+    public void movePaddle(int x){
+    	if(paddle.position.x+paddle.bounds.width+x>=w||paddle.position.x+x<=0) return;
+    	
+    	paddle.position.x+=x;
+    }
 
     @Override
     public void create() {
 
+    }
+    
+    public void update(){
+    	// Update the positions of game objects
+        w = Gdx.graphics.getWidth();
+    	h = Gdx.graphics.getHeight();
+    	for(Ball ball:balls){
+    		moveBall(ball,ball.velocity);
+    	}
+    	boundsTestBalls();
+    	bounceTest();
+    }
+    
+    private void boundsTestBalls(){
+    	// Keep the ball within the boundaries of the game
+    	for(Ball ball:balls){
+        	if(ball.position.x+ball.bounds.width>=w){
+        		ball.velocity.x = -ball.velocity.x;
+        	}
+        	if(ball.position.y+ball.bounds.height>=h){
+        		ball.velocity.y = -ball.velocity.y;
+        	}
+        	if(ball.position.x<=0){
+        		ball.velocity.x = -ball.velocity.x;
+        	}
+        	if(ball.position.y<=0){
+        		ball.velocity.y = -ball.velocity.y;
+        	}
+		
+    	}
+    	
+    }
+    
+    private void bounceTest(){
+    	
     }
 }
--- BlocBreaker/src/com/richardeh/blocbreaker/Screens/GameScreen.java
+++ BlocBreaker/src/com/richardeh/blocbreaker/Screens/GameScreen.java
@@ -1,9 +1,13 @@
 package com.richardeh.blocbreaker.Screens;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.richardeh.blocbreaker.BlocBreaker;
 import com.richardeh.blocbreaker.Game.Assets;
 import com.richardeh.blocbreaker.Game.Game;
@@ -34,16 +38,28 @@ public class GameScreen extends GenericScreen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        ballSprite.setPosition(game.getBallPosition().x,game.getBallPosition().y);
-        paddleSprite.setPosition(game.getPaddlePosition().x,game.getPaddlePosition().y);
+		ArrayList<Vector2> balls = game.getBallPosition();
+		game.update();
+		paddleSprite.setPosition(game.getPaddlePosition().x,game.getPaddlePosition().y);
 		batch.begin();
-        ballSprite.draw(batch);
+
+		for(Vector2 v:balls){
+	        ballSprite.setPosition(v.x,v.y);
+	        ballSprite.draw(batch);	
+		}
         paddleSprite.draw(batch);
 		batch.end();
+		
 	}
 	
 	public boolean keyUp(int keycode){
 		
+		return false;
+	}
+	
+	public boolean keyDown(int keycode){
+		if(keycode == Keys.RIGHT) game.movePaddle(5);
+		if(keycode == Keys.LEFT) game.movePaddle(-5);
 		return false;
 	}
 }
